cdfd8d22fcad6ac7c7656a7b7f6664c44b93d7ee
GYauck <yauckgaston@gmail.com>
2023-01-15 18:03:52 -0300

    destination page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Destination from "./Pages/Destination/Destination";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <Routes>
-      <Route  path="/" element={<Home/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/destination" element={<Destination />} />
     </Routes>
   );
 }
