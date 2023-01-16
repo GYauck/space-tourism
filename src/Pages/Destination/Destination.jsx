01f9221e3f071509bfc8b796cf0c52d1ef8eec81
GYauck <yauckgaston@gmail.com>
2023-01-16 11:46:57 -0300

    added active&hover destination navbar

--- src/Pages/Destination/Destination.jsx
+++ src/Pages/Destination/Destination.jsx
@@ -3,53 +3,64 @@ import Navbar from "../../Components/Navbar/Navbar";
 import style from "./destination.module.css";
 import data from "../../data.json";
 
-
 const Destination = () => {
+  const [planets] = useState(data.destinations);
 
-  const [planets] = useState(data.destinations)
-  
   //console.log(planets[0].images.webp)
+
+  const [value, setValue] = useState(0);
+  const [isActive, setActive] = useState();
+  const { name, images, description, distance, travel } = planets[value];
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(event.target.id)
+    setActive(event.target.id);
+  }
+
   
-  const [value, setValue] = useState(0)
-  const { name, images, description, distance, travel } = planets[value]
-  console.log(images.png)
   return (
     <div className={style.background}>
       <Navbar />
-    <div className={style.fullContainer}>
-      <div className={style.leftContainer}>
-        <div className={style.titleContainer}>
-          <h2 className={style.title}>01<span className={style.titleSpan}>PICK YOUR DESTINATION</span> </h2>
-          <img className={style.planetImg} src={images.png} alt="image" />
+      <div className={style.fullContainer}>
+        <div className={style.leftContainer}>
+          <div className={style.titleContainer}>
+            <h2 className={style.title}>
+              01 <span className={style.titleSpan}>PICK YOUR DESTINATION</span>{" "}
+            </h2>
+            <img className={style.planetImg} src={images.png} alt="image" />
+          </div>
+          <div className={style.rigthContainer}>
+            <ul className={style.ulNav}>
+              {planets.map((planet, index) => (
+                <li key={index}>
+                  <button id={index}
+                    className={isActive == index ? style.activeButtom : style.ulButton}
+                    onClick={(e) => {
+                      handleClick(e)
+                      setValue(index)
+                    }}
+                  >
+                    {planet.name.toUpperCase()}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <h2 className={style.planetName}>{name}</h2>
+            <p className={style.pDescription}>{description}</p>
+            {/* <hr /> */}
+            <ul className={style.ulDescription}>
+              <li className={style.liDistance}>
+                <span>AVG. DISTANCE</span>{" "}
+                <span className={style.spanDescription}>{distance}</span>
+              </li>
+              <li className={style.liTravel}>
+                EST. TRAVEL TIME{" "}
+                <span className={style.spanDescription}>{travel.toUpperCase()}</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className={style.rigthContainer}>
-        <ul className={style.ulNav}>
-          {planets.map((planet, index) => 
-            <li key={index} >
-              <button className={style.ulButton} onClick={()=> setValue(index)}>
-                {planet.name.toUpperCase()}
-              </button>
-            </li>
-          )}
-        </ul>
-        <h2 className={style.planetName}>
-          {name}
-        </h2>
-        <p className={style.pDescription}>
-          {description}
-        </p>
-        {/* <hr /> */}
-        <ul className={style.ulBottom}>
-          <li className={style.liDistance}>
-            <span>AVG. DISTANCE</span>  <span className={style.spanBottom}>{distance}</span>
-          </li>
-          <li className={style.liTravel}>
-            EST. TRAVEL TIME <span className={style.spanBottom}>{travel}</span>
-          </li>
-        </ul>
-        </div>
-          
-      </div>
       </div>
     </div>
   );
