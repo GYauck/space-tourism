efd882b00bf31e8e17f5e1b5463131595cc83520
GYauck <yauckgaston@gmail.com>
2023-01-18 21:08:02 -0300

    technology page WIP

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Crew from "./Pages/Crew/Crew";
 import Destination from "./Pages/Destination/Destination";
 import Home from "./Pages/Home/Home";
+import Technology from "./Pages/Technology/Technology";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/destination" element={<Destination />} />
-      <Route path="crew" element={<Crew />} />
+      <Route path="/crew" element={<Crew />} />
+      <Route path="/technology" element={<Technology />} />
     </Routes>
   );
 }
---
+++ src/Pages/Technology/Technology.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import style from "./technology.module.css";
+import data from "../../data.json";
+
+const Technology = () => {
+  const [technology] = useState(data.technology);
+  const [value, setValue] = useState(0);
+  const [isActive, setActive] = useState(0);
+  const { name, images, description } = technology[value];
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(event.target.id);
+    setActive(event.target.id);
+  };
+
+  return (
+    <div className={style.background}>
+      <Navbar />
+      <div className={style.fullContainer}>
+        <div className={style.leftContainer}>
+          <h1 className={style.title}>
+            03 <span className={style.titleSpan}> SPACE LAUNCH 101</span>
+          </h1>
+          <div className={style.ulAndContent}>
+            <ul className={style.ulNav}>
+              {technology.map((technology, index) => (
+                <li key={index}>
+                  <button
+                    id={index}
+                    className={
+                      isActive == index ? style.activeButtom : style.ulButton
+                    }
+                    onClick={(e) => {
+                      handleClick(e);
+                      setValue(index);
+                    }}
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              ))}
+            </ul>
+
+            <div className={style.nameAndDescriptionContainer}>
+              <h2 className={style.subTitle}>THE TERMINOLOGY</h2>
+              <h3 className={style.name}>{name.toUpperCase()}</h3>
+              <p className={style.description}>{description}</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.rightContainer}>
+          <img src={images.portrait} alt="TechnologyImg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technology;
