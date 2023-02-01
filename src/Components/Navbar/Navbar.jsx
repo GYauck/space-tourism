625cfd42ca2c033cdeeab425ffb6d852d0ecd767
GYauck <yauckgaston@gmail.com>
2023-01-31 21:23:42 -0300

    HamburguerNav & phone responsive WIP

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import Logo from "../../assets/shared/logo.svg";
+import HamburguerLogo from "../../assets/shared/icon-hamburger.svg"
+import XLogo from "../../assets/shared/icon-close.svg"
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   let activeClassName = "active";
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
     <nav>
@@ -14,7 +18,57 @@ const Navbar = () => {
         </div>
         <hr />
 
+        {/* Normal Nav */}
         <ul className="navLinks">
+          <li className="navText">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              00 HOME
+            </NavLink>
+          </li>
+          <li className="navText">
+            <NavLink
+              to={"/destination"}
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              01 DESTINATION
+            </NavLink>
+          </li>
+          <li className="navText">
+            <NavLink
+              to={"/crew"}
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              02 CREW
+            </NavLink>
+          </li>
+          <li className="navText">
+            <NavLink
+              to={"/technology"}
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              03 TECHNOLOGY
+            </NavLink>
+          </li>
+        </ul>
+
+        {/* Hamburger */}
+        <div onClick={handleClick} className="hamburguerIcon">
+          {!nav ? <img src={HamburguerLogo} alt="Hamburguer" /> : <img src={XLogo} alt="X"/>}
+        </div>
+
+        {/* Mobile Nav */}
+        <ul className={!nav ? "mobileMenu" : "mobileMenuDisplay"}>
           <li className="navText">
             <NavLink
               to={"/"}
